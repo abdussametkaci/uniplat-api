e35bbcc11cc6eab55176887fbde7188bf3ee536e
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-01-16 00:38:06 +0300

    add heroku database configs

--- src/main/kotlin/com/uniplat/uniplatapi/configuration/FlywayConfiguration.kt
+++ src/main/kotlin/com/uniplat/uniplatapi/configuration/FlywayConfiguration.kt
@@ -16,7 +16,8 @@ class FlywayConfiguration(private val databaseProperties: DatabaseProperties) {
                     .baselineOnMigrate(true)
                     .outOfOrder(true)
                     .dataSource(
-                        getDatabaseUrl(host, port, name),
+                        //getDatabaseUrl(host, port, name),
+                        getHerokuDatabaseURL(),
                         username,
                         password
                     )
@@ -26,5 +27,9 @@ class FlywayConfiguration(private val databaseProperties: DatabaseProperties) {
 
     fun getDatabaseUrl(host: String, port: String, name: String): String {
         return "jdbc:postgresql://$host:$port/$name"
+    }
+
+    fun getHerokuDatabaseURL(): String {
+        return "postgres://naeneeqjlpzqvc:bf5aceaccdbd12f84c92138f3de9a293935c47693c69b393569a1fb8e3febc91@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/d6c4h4ktogn7nf"
     }
 }
