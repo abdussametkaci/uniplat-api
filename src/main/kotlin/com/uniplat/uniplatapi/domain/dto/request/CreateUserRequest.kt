ba731a9f84dd9831f9a28b7c75aae1a6e47b39bf
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-03-14 23:13:23 +0300

    add validation for create and update user

--- src/main/kotlin/com/uniplat/uniplatapi/domain/dto/request/CreateUserRequest.kt
+++ src/main/kotlin/com/uniplat/uniplatapi/domain/dto/request/CreateUserRequest.kt
@@ -4,19 +4,30 @@ import com.uniplat.uniplatapi.domain.enums.Gender
 import java.time.Instant
 import java.util.UUID
 import javax.validation.constraints.Email
+import javax.validation.constraints.Size
+import kotlin.math.min
 
 data class CreateUserRequest(
+    @get:Size(min = 1, max = 255, message = "error.user.name-invalid")
     val name: String,
+
+    @get:Size(min = 1, max = 255, message = "error.user.surname-invalid")
     val surname: String,
+
     val gender: Gender,
     val birthDate: Instant,
 
     @get:Email(message = "error.email.invalid")
     val email: String,
 
+    @get:Size(min = 4, max = 255, message = "error.user.password-invalid")
     val password: String,
+
     val universityId: UUID,
+
+    @get:Size(min = 1, max = 255, message = "error.user.description-invalid")
     val description: String?,
+
     val profileImgId: UUID?,
     val messageAccessed: Boolean = true
 )
--- src/main/kotlin/com/uniplat/uniplatapi/domain/dto/request/UpdateUserRequest.kt
+++ src/main/kotlin/com/uniplat/uniplatapi/domain/dto/request/UpdateUserRequest.kt
@@ -3,15 +3,26 @@ package com.uniplat.uniplatapi.domain.dto.request
 import com.uniplat.uniplatapi.domain.enums.Gender
 import java.time.Instant
 import java.util.UUID
+import javax.validation.constraints.Size
 
 data class UpdateUserRequest(
+    @get:Size(min = 1, max = 255, message = "error.user.name-invalid")
     val name: String?,
+
+    @get:Size(min = 1, max = 255, message = "error.user.surname-invalid")
     val surname: String?,
+
     val gender: Gender?,
     val birthDate: Instant?,
+
+    @get:Size(min = 4, max = 255, message = "error.user.password-invalid")
     val password: String?,
+
     val universityId: UUID?,
+
+    @get:Size(min = 1, max = 255, message = "error.user.description-invalid")
     val description: String?,
+
     val profileImgId: UUID?,
     val messageAccessed: Boolean?,
     val version: Int
