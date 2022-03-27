1a6b6c5315885a7a8efc86ca3182d66b804037d4
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-03-27 21:20:22 +0300

    update university sql

--- src/main/resources/db/migration/postgresql/V2__init_university.sql
+++ src/main/resources/db/migration/postgresql/V2__init_university.sql
@@ -1,9 +1,11 @@
 CREATE TABLE university
 (
-    id               UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
-    name             VARCHAR(255) UNIQUE             NOT NULL,
-    admin_id         UUID                            NOT NULL,
-    version          INTEGER                         NOT NULL,
-    created_at       TIMESTAMP WITHOUT TIME ZONE     NOT NULL,
-    last_modified_at TIMESTAMP WITHOUT TIME ZONE     NOT NULL
+    id               UUID                        NOT NULL DEFAULT uuid_generate_v4(),
+    name             VARCHAR(255)                NOT NULL,
+    admin_id         UUID                        NOT NULL,
+    version          INTEGER                     NOT NULL,
+    created_at       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    last_modified_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (name)
 );
