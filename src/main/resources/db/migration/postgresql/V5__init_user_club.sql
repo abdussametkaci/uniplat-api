5200cebcbb9791fe809ccb562b02ecd243847052
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-04-24 14:06:25 +0300

    format fields

--- src/main/resources/db/migration/postgresql/V5__init_user_club.sql
+++ src/main/resources/db/migration/postgresql/V5__init_user_club.sql
@@ -1,11 +1,11 @@
 CREATE TABLE user_club
 (
     id               UUID                        NOT NULL DEFAULT uuid_generate_v4(),
-    club_id          UUID                        NOT NULL,
     user_id          UUID                        NOT NULL,
+    club_id          UUID                        NOT NULL,
     version          INTEGER                     NOT NULL,
     created_at       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     last_modified_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE (club_id, user_id)
+    UNIQUE (user_id, club_id)
 );
