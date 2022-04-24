55c464433b1843735fa7bc945820642f01cad985
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-04-24 14:21:20 +0300

    renamed user university from university user

--- src/main/kotlin/com/uniplat/uniplatapi/domain/mapper/UserUniversityResponseMapper.kt
+++ src/main/kotlin/com/uniplat/uniplatapi/domain/mapper/UserUniversityResponseMapper.kt
@@ -1,9 +1,9 @@
 package com.uniplat.uniplatapi.domain.mapper
 
-import com.uniplat.uniplatapi.domain.dto.response.UniversityUserResponse
+import com.uniplat.uniplatapi.domain.dto.response.UserUniversityResponse
 import com.uniplat.uniplatapi.domain.model.UserUniversity
 import org.mapstruct.Mapper
 import org.springframework.core.convert.converter.Converter
 
 @Mapper
-interface UserUniversityResponseMapper : Converter<UserUniversity, UniversityUserResponse>
+interface UserUniversityResponseMapper : Converter<UserUniversity, UserUniversityResponse>
