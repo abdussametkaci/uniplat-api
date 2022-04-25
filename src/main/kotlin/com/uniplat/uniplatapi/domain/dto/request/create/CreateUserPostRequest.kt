f8b1c5aa9acff4a5c8b4c35e6ced26668cdb0d9a
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-04-25 22:54:32 +0300

    delete unnecessary controller (user post)

--- src/main/kotlin/com/uniplat/uniplatapi/domain/dto/request/create/CreateUserPostRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.uniplat.uniplatapi.domain.dto.request.create
-
-import java.util.UUID
-
-data class CreateUserPostRequest(
-    val userId: UUID,
-    val postId: UUID
-)
--- src/main/kotlin/com/uniplat/uniplatapi/domain/mapper/UserPostResponseMapper.kt
+++ src/main/kotlin/com/uniplat/uniplatapi/domain/mapper/UserPostResponseMapper.kt
@@ -1,7 +1,6 @@
 package com.uniplat.uniplatapi.domain.mapper
 
 import com.uniplat.uniplatapi.domain.dto.response.UserPostResponse
-import com.uniplat.uniplatapi.domain.model.UserPost
 import org.mapstruct.Mapper
 import org.springframework.core.convert.converter.Converter
 
