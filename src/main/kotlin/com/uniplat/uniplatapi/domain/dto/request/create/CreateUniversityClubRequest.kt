2dc1d9964d788016906c508aab23c4cfcc5a4d13
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-04-25 23:07:58 +0300

    delete unnecessary university club

--- src/main/kotlin/com/uniplat/uniplatapi/domain/dto/request/create/CreateUniversityClubRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.uniplat.uniplatapi.domain.dto.request.create
-
-import java.util.UUID
-
-data class CreateUniversityClubRequest(
-    val universityId: UUID,
-    val clubId: UUID
-)
--- src/main/kotlin/com/uniplat/uniplatapi/domain/mapper/UserPostResponseMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.uniplat.uniplatapi.domain.mapper
-
-import com.uniplat.uniplatapi.domain.dto.response.UserPostResponse
-import org.mapstruct.Mapper
-import org.springframework.core.convert.converter.Converter
-
-@Mapper
-interface UserPostResponseMapper : Converter<UserPost, UserPostResponse>
