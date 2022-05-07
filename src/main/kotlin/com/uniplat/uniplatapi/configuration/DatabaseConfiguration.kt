cf1dfcd231485f7e9abfd5dc86e9ae4cba82758c
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-05-07 15:11:23 +0300

    delete abstract r2dbc db configuration - add database client and database template for custom native query and assigned joined data to a DTO

--- src/main/kotlin/com/uniplat/uniplatapi/configuration/DatabaseConfiguration.kt
+++ src/main/kotlin/com/uniplat/uniplatapi/configuration/DatabaseConfiguration.kt
@@ -10,20 +10,28 @@ import io.r2dbc.postgresql.PostgresqlConnectionConfiguration
 import io.r2dbc.postgresql.PostgresqlConnectionFactory
 import io.r2dbc.postgresql.codec.EnumCodec
 import io.r2dbc.postgresql.extension.CodecRegistrar
+import io.r2dbc.spi.ConnectionFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 import org.springframework.data.r2dbc.convert.EnumWriteSupport
+import org.springframework.data.r2dbc.core.DefaultReactiveDataAccessStrategy
+import org.springframework.data.r2dbc.core.R2dbcEntityOperations
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
+import org.springframework.data.r2dbc.dialect.PostgresDialect
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.r2dbc.core.DatabaseClient
 
 @Configuration
-@EnableR2dbcRepositories
+@EnableR2dbcRepositories(
+    entityOperationsRef = "databaseTemplate",
+    basePackages = ["com.uniplat.uniplatapi.repository"]
+)
 @EnableR2dbcAuditing
-class DatabaseConfiguration(private val databaseProperties: DatabaseProperties) : AbstractR2dbcConfiguration() {
+class DatabaseConfiguration(private val databaseProperties: DatabaseProperties) {
 
     @Bean
-    override fun connectionFactory(): PostgresqlConnectionFactory {
+    fun connectionFactory(): PostgresqlConnectionFactory {
         return PostgresqlConnectionFactory(
             with(databaseProperties) {
                 PostgresqlConnectionConfiguration.builder()
@@ -38,6 +46,20 @@ class DatabaseConfiguration(private val databaseProperties: DatabaseProperties) 
         )
     }
 
+    @Bean
+    fun databaseClient(connectionFactory: ConnectionFactory): DatabaseClient {
+        return DatabaseClient.builder()
+            .connectionFactory(connectionFactory)
+            .bindMarkers(PostgresDialect.INSTANCE.bindMarkersFactory)
+            .build()
+    }
+
+    @Bean
+    fun databaseTemplate(databaseClient: DatabaseClient): R2dbcEntityOperations {
+        val strategy = DefaultReactiveDataAccessStrategy(PostgresDialect.INSTANCE, getCustomConverters())
+        return R2dbcEntityTemplate(databaseClient, strategy)
+    }
+
     fun getCodecRegistrar(): CodecRegistrar {
         return EnumCodec.builder()
             .withEnum("gender", Gender::class.java)
@@ -47,7 +69,7 @@ class DatabaseConfiguration(private val databaseProperties: DatabaseProperties) 
             .build()
     }
 
-    override fun getCustomConverters(): List<EnumWriteSupport<out Enum<*>>> {
+    fun getCustomConverters(): List<EnumWriteSupport<out Enum<*>>> {
         return listOf(
             enumConverterOf<Gender>(),
             enumConverterOf<UserType>(),
