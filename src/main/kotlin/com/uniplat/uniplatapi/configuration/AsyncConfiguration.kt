5bcf5b4c3cc9537eac26eaf259f7c4a3dcb8977d
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-05-16 13:02:42 +0300

    create async configuration

---
+++ src/main/kotlin/com/uniplat/uniplatapi/configuration/AsyncConfiguration.kt
@@ -0,0 +1,18 @@
+package com.uniplat.uniplatapi.configuration
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.Executors
+
+@Configuration
+class AsyncConfiguration {
+
+    @Bean
+    fun asyncDispatcher(): CoroutineDispatcher = Executors.newFixedThreadPool(10).asCoroutineDispatcher()
+
+    @Bean
+    fun applicationScope(asyncDispatcher: CoroutineDispatcher): CoroutineScope = CoroutineScope(asyncDispatcher)
+}
