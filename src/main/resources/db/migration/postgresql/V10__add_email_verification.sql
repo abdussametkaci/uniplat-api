513c34e8bb80c01478cd724dc5c7a7eab31c4307
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-06-05 11:43:59 +0300

    format email verification code sql and user enabled column is not null

--- src/main/resources/db/migration/postgresql/V10__add_email_verification.sql
+++ src/main/resources/db/migration/postgresql/V10__add_email_verification.sql
@@ -1,11 +1,11 @@
 ALTER TABLE "user"
-    ADD COLUMN enabled BOOLEAN;
+    ADD COLUMN enabled BOOLEAN DEFAULT FALSE NOT NULL;
 
 CREATE TABLE email_verification_code
 (
     id               UUID                        NOT NULL DEFAULT uuid_generate_v4(),
     user_id          UUID                        NOT NULL,
-    code             VARCHAR(64)                NOT NULL,
+    code             VARCHAR(64)                 NOT NULL,
     created_at       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     last_modified_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     PRIMARY KEY (id),
